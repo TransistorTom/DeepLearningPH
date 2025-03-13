5ee3c099984bf68f5f07f42e8f8b08076b3eac6d
Jacob Toot <jacobtoot@icloud.com>
2025-03-13 21:41:42 +0100

    Update monkey.py
    added more functionality

--- monkey.py
+++ monkey.py
@@ -4,10 +4,17 @@ class Monkey:
         self.age = age    # Instance variable for monkey's age
 
     def introduce(self):
-        print(f"Hi, I'm {self.name}, and I'm {self.age} years old!")
+        print(f"Hi, I'm {self.name}, I'm {self.age} years old, and I love {self.favorite_tree} trees!")
+        
+    def celebrate_birthday(self):
+        self.age += 1  # Modify THIS monkey's age
+        print(f"Happy birthday, {self.name}! You are now {self.age} years old!")
 
-# Creating an instance of Monkey
-gibbon = Monkey("Gibby", 5)
+    def swing(self):
+        print(f"{self.name} is swinging through the trees!")
 
-# Calling the method to introduce the monkey
-gibbon.introduce()
+    
+# Creating an instance of Monkey (Feel free to add your own monkey to the jungle! give it a "name", age, "favourite_tree")
+gibbon = Monkey("Gibby", 5, "banyan")
+lemur = Monkey("Momo", 3, "fig")
+panda = Monkey("Xi Lan", 12, "bamboo")
