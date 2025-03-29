c282e8b9d10ed670ecef3ebef03899c5f57bc9fa
Jacob Toot <jacobtoot@icloud.com>
2025-03-29 23:31:07 +0100

    cuda?

--- SLURM_scripts/pipeline_runner_slurm.py
+++ SLURM_scripts/pipeline_runner_slurm.py
@@ -9,14 +9,14 @@ from functions.train_model import train_model
 from functions.pipeline import pipeline
 import datetime
 
-repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+repo_root = os.getwcd()
 if repo_root not in sys.path:
     sys.path.insert(0, repo_root)
 
 
 if __name__ == "__main__":
     # Creating folders for results on habrok
-    repo_dir = os.path.dirname(os.path.abspath(__file__))
+    repo_dir = os.getwcd()
     job_id = os.environ.get("SLURM_JOB_ID", "nojob")
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     results_dir = os.path.join(repo_dir, f"results/job{job_id}_{timestamp}")
@@ -26,8 +26,8 @@ if __name__ == "__main__":
     N = 3
     # Running full model for training
     model, train_df, test_dfs, history_loss = pipeline(
-        train_iterations=100,
-        test_iterations=20,
+        train_iterations=10,
+        test_iterations=2,
         N_train=N,
         N_test_list=[3, 4, 5],
         T=100,
